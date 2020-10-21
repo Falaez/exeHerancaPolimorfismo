d667f2947a76aeb53d2550d5c40ac4907329a952
Falaez <mariohenriquelp@gmail.com>
2020-10-21 10:43:22 -0300

    100% exe completo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,10 +10,11 @@ import java.util.Scanner;
 
 import entities.ImportedProduct;
 import entities.Product;
+import entities.UsedProduct;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
@@ -22,29 +24,38 @@ public class Program {
 		System.out.print("Enter the number of products: ");
 		int n = sc.nextInt();
 		
-		for (int i=1; i<n; i++) {
-			System.out.println("Product #"+i+"data:");
+		for (int i=1; i<=n; i++) {
+			System.out.println("Product #"+i+" data:");
 			System.out.print("Common, used or imported (c/u/i): ");
 			sc.nextLine();
 			char ch = sc.next().charAt(0);
 			System.out.print("Name:");
+			sc.nextLine();
 			String name = sc.nextLine();
-			System.out.println("Price:");
+			System.out.print("Price:");
 			Double price = sc.nextDouble();
 			if (ch == 'i') {
-				System.out.println("Custom fee: ");
+				System.out.print("Custom fee: ");
 				Double customsFee = sc.nextDouble();
-				product.add(new ImportedProduct(name, price, customsFee)r);
+				product.add(new ImportedProduct(name, price, customsFee));
 			}
-			if (ch == 'n') {
+			else if (ch == 'u'){
+				System.out.print("Manufacture date: ");
+				Date manufactureDate = sdf1.parse(sc.next());
+				product.add(new UsedProduct(name, price, manufactureDate));
+			}
+			else{
 				product.add(new Product(name, price));
 			}
-			if (ch == 'u') {
-				System.out.println("Manufacture date: ");
-				Date manufactureDate = sdf1.parse(sc.nextLine());
-				
-			}
 		}
+		System.out.println();
+		System.out.println("PRICE TAGS: ");
+		for (Product p: product){
+			System.out.println(p.priceTag());
+			
+		}
+		
+			
 		
 		sc.close();
 	}
